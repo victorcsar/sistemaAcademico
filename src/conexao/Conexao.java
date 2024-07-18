2e51b0e08e13185deedc9fb546d3c96c16bc954c
victorcsar <victorcesagx@gmail.com>
2024-07-18 20:40:13 -0300

    att pass

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String url = "jdbc:mysql://localhost:3306/projeto";
     private static final String user = "root";
-    private static final String password = "modificaraqui";
+    private static final String password = "senha";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(url, user, password);
